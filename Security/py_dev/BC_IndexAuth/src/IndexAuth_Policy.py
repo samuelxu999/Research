784f45e12a4dc2201bc5ec80cfdbaf1b5835ed1e
samuelxu999 <samuelxu999@gmail.com>
2018-07-02 20:01:38 -0400

    Add Index_Token.py and update IndexAuth_Policy.py

--- Security/py_dev/BC_IndexAuth/src/IndexAuth_Policy.py
+++ Security/py_dev/BC_IndexAuth/src/IndexAuth_Policy.py
@@ -12,11 +12,36 @@ Created on July.2, 2018
 
 from utilities import DatetimeUtil, TypesUtil, FileUtil
 from wrapper_pyca import Crypto_Hash
+from Index_Token import IndexToken
+
+
+#global variable
+http_provider = 'http://localhost:8042'
+contract_addr = '0xb71e920f014cddc0064ed166e10a0126ef76f537'
+contract_config = '../IndexAuthContract/build/contracts/IndexToken.json'
+
+#new CapACToken object
+mytoken=IndexToken(http_provider, contract_addr, contract_config)
+
 
 '''
-Get all dataset
+Index authentication policy management
 '''
-def test_func():
+class IndexPolicy(object):
+
+	# get token data from smart contract, return json fromat
+	@staticmethod
+	def get_indexToken(str_index):
+		token_data=mytoken.getIndexToken(str_index);
+		json_token={}
+
+		#Add status information
+		json_token['id'] = token_data[0]
+		json_token['HashValue'] = token_data[1]
+
+		return json_token
+
+def test_pyca():
 	# transfer string data to bytes block
 	bytes_block=TypesUtil.string_to_bytes('samuel');
 
@@ -26,5 +51,34 @@ def test_func():
 	
 	
 if __name__ == "__main__":
-	test_func()
+	#set sample record 
+	record_block={}
+	record_block['id']='1'
+	record_block['value']='samuelxu'
+
+	#1) read token data
+	token_data=mytoken.getIndexToken(record_block['id']);
+	print(token_data)
+
+	#2) get hash value for index
+	# transfer string data to bytes block
+	bytes_block = TypesUtil.string_to_bytes(record_block['value']);
+
+	hash_value = Crypto_Hash.generate_hash(bytes_block)
+	#hash_str = TypesUtil.bytes_to_string(hash_value)
+	print(hash_value)
+	#print(hash_str)
+	print(token_data[1])
+
+	print(str(hash_value)==token_data[1])
+
+	#3) set index token
+	#mytoken.setIndexToken(record_block['id'], hash_str);
+
+	#verify hash
+	#hash_token = token_data[1]
+	#print(Crypto_Hash.verify_hash(hash_value, bytes_block))
+
+	#json_token = IndexPolicy.get_indexToken('1');
+	#print(json_token)
 	pass
---
+++ Security/py_dev/BC_IndexAuth/src/Index_Token.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3.5
+
+'''
+========================
+Index token module
+========================
+Created on July.02, 2018
+@author: Xu Ronghua
+@Email:  rxu22@binghamton.edu
+@TaskDescription: This module provide encapsulation of web3.py API to interact with IndexToken.sol smart contract.
+'''
+
+from web3 import Web3, HTTPProvider, IPCProvider
+from utilities import DatetimeUtil, TypesUtil
+import json, datetime, time
+
+class IndexToken(object):
+	def __init__(self, http_provider, contract_addr, contract_config):
+		# configuration initialization
+		self.web3 = Web3(HTTPProvider(http_provider))
+		self.contract_address=Web3.toChecksumAddress(contract_addr)
+		self.contract_config = json.load(open(contract_config))
+
+		# new contract object
+		self.contract=self.web3.eth.contract()
+		self.contract.address=self.contract_address
+		self.contract.abi=self.contract_config['abi']
+
+	def Show_ContractInfo(self):  
+		print(self.web3.eth.blockNumber)
+		print(self.web3.eth.accounts)
+		print(self.web3.fromWei(self.web3.eth.getBalance(self.web3.eth.accounts[0]), 'ether'))
+
+		print(self.contract.address)
+
+	# return accounts address
+	def getAccounts(self):
+		return self.web3.eth.accounts
+
+	# return accounts balance
+	def getBalance(self, account_addr=''):
+		if(account_addr==''):
+			# get accounts[0] balance
+			checksumAddr=self.web3.eth.coinbase
+		else:
+			#Change account address to EIP checksum format
+			checksumAddr=Web3.toChecksumAddress(account_addr)	
+		return self.web3.fromWei(self.web3.eth.getBalance(checksumAddr), 'ether')
+
+	#get token data by call getIndexToken()
+	def getIndexToken(self, str_index):
+
+		token_data = []
+		'''
+		Call a contract function, executing the transaction locally using the eth_call API. 
+		This will not create a new public transaction.
+		'''
+
+		# get token status
+		token_data=self.contract.call({'from': self.web3.eth.coinbase}).getIndexToken(str_index)
+
+		return token_data
+
+	#initialized token by sending transact to call initIndexToken()
+	def initIndexToken(self, str_index):
+
+		# Execute the specified function by sending a new public transaction.
+		ret=self.contract.transact({'from': self.web3.eth.coinbase}).initIndexToken(self.web3.eth.coinbase, str_index)
+
+	# set isValid flag in token
+	def setIndexToken(self, str_index, hash_index):
+
+		# Execute the specified function by sending a new public transaction.	
+		ret=self.contract.transact({'from': self.web3.eth.coinbase}).setIndexToken(self.web3.eth.coinbase, str_index, hash_index)
+
+	# Print token date
+	@staticmethod
+	def print_tokendata(token_data):
+		#print token status
+		for i in range(0,len(token_data)):
+				print(token_data[i])
+
+
+	# get address from json file
+	@staticmethod
+	def getAddress(node_name, datafile):
+		address_json = json.load(open(datafile))
+		return address_json[node_name]
+
+
+if __name__ == "__main__":
+	http_provider = 'http://localhost:8042'
+	contract_addr = '0xb71e920f014cddc0064ed166e10a0126ef76f537'
+	contract_config = '../IndexAuthContract/build/contracts/IndexToken.json'
+
+	#Get account address
+	accountAddr=IndexToken.getAddress('sam_miner_win7_0', '../IndexAuthContract/test/addr_list.json')
+	print("Account: " + accountAddr)
+	#new ABACToken object
+	mytoken=IndexToken(http_provider, contract_addr, contract_config)
+	#mytoken.Show_ContractInfo()
+
+
+	#------------------------- test contract API ---------------------------------
+	#getAccounts
+	accounts = mytoken.getAccounts()
+	balance = mytoken.getBalance('0x950d8eb4825c597534027638c862496ea0d7cf43')
+	print(accounts)
+	print(balance)
+
+	#Read token data using call
+	token_data=mytoken.getIndexToken('1');
+	IndexToken.print_tokendata(token_data)
+
+	#Send transact
+	#mytoken.initIndexToken('1');
+	#mytoken.setIndexToken('1', 'dave')
+
+	pass
