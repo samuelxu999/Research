7bfdf91d7ff32da36841cd45bde832dbe0bcbe9a
samuelxu999 <rxu22@binghamton.edu>
2021-05-11 18:20:41 -0400

    Update service_run.sh

--- Security/py_dev/ENF_chain/DOCKER/service_run.sh
+++ Security/py_dev/ENF_chain/DOCKER/service_run.sh
@@ -5,8 +5,10 @@
 
 OPERATION=$1
 CONTAINER_NAME=$2
-RPC_PORT=$3
-PORT=$4
+IMAGE_TYPE=$3
+RPC_PORT=$4
+PORT=$5
+BOOT_STRAP=$6
 
 ## Check container name
 if [[ "" == $2 ]]; then
@@ -14,8 +16,8 @@ if [[ "" == $2 ]]; then
 	echo "Use default container name: $CONTAINER_NAME"
 fi
 
-# Start container
-if  [ "start" == "$OPERATION" ] ; then
+## Start container
+if  [ "start" == "$OPERATION" ]; then
 	echo "Startup service!"
 
 	if ! [[ $RPC_PORT =~ ^[0-9]+$ ]]; then
@@ -28,18 +30,44 @@ if  [ "start" == "$OPERATION" ] ; then
 		exit 0
 	fi
 
+	## Check image type name
+	if [ "x86" == $IMAGE_TYPE ]; then
+		echo "Use x86 version"
+		IMAGE_FILE="samuelxu999/ecoledger_node:x86"
+	elif [ "arm" == $IMAGE_TYPE ]; then
+		echo "Use armv7l version"
+		IMAGE_FILE="samuelxu999/ecoledger_node:armv7l"
+	else
+		echo "Not support image version."
+		exit 0
+	fi
+	## prepare docker image
+	docker pull "$IMAGE_FILE"
+	docker tag "$IMAGE_FILE" ecoledger_node
+
 	# bootup container node
 	./run_node.sh start $CONTAINER_NAME
 
-	# run geth as node
-	./docker_exec.sh $CONTAINER_NAME docker "python3 /home/docker/app/ENFChain_server.py --blockepoch 3 --pauseepoch 3 --phasedelay 2 -p $PORT --rp $RPC_PORT --refresh_neighbors 15" &>/dev/null &
+	if [ "" == "$BOOT_STRAP" ]; then
+		echo "Run as boot strap node: 0.0.0.0:$RPC_PORT"
+		CMD_Line="python3 /home/docker/app/ENFChain_server.py --blockepoch 3 --pauseepoch 3 --phasedelay 2 -p $PORT --rp $RPC_PORT --firstnode --save_state 60"
+	else
+		echo "Run as ENF node to contact: $BOOT_STRAP"
+		CMD_Line="python3 /home/docker/app/ENFChain_server.py --blockepoch 3 --pauseepoch 3 --phasedelay 2 -p $PORT --rp $RPC_PORT --refresh_neighbors 15 --bootstrapnode $BOOT_STRAP"
+	fi
+	## run ENF service app
+	./docker_exec.sh $CONTAINER_NAME docker "$CMD_Line" &>/dev/null &
+	
 
-# Stop container
-elif [ "stop" == "$OPERATION" ] ; then
+## Stop container
+elif [ "stop" == "$OPERATION" ]; then
 	echo "Stop running service!"
 	./run_node.sh stop $CONTAINER_NAME
-# List container
-else
+## List container
+elif [ "show" == "$OPERATION" ]; then
 	echo "Show running service!"
-	./run_node.sh list
+	docker container ls
+## show usage
+else
+	echo "Usage $0 list|start|show| -image_type(x86|armv7l) -container_name rpc_port port bootstrapnode_ip:port"
 fi
