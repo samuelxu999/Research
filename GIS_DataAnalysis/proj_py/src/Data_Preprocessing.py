d5714023d533380ebccc4372035edf655ab07d67
samuelxu999 <rxu22@binghamton.edu>
2021-11-17 14:24:01 -0500

    Update Data_Preprocessing.py and add test cases in test.py

--- GIS_DataAnalysis/proj_py/src/Data_Preprocessing.py
+++ GIS_DataAnalysis/proj_py/src/Data_Preprocessing.py
@@ -9,7 +9,7 @@ Created on Jan.20, 2020
 @Reference:
 '''
 
-import time, math, os
+import math, os
 from datetime import datetime
 import numpy as np
 import gdal
@@ -90,20 +90,6 @@ def date_Conversion(Day_Original):
 	Julian_Day = Date_value.toordinal()
 	return Julian_Day
 
-def raster2array(rasterfn, isAlldata=True):
-	'''
-	Function: demo and test reading file functions of gdal.
-	@rasterfn: 		original raster file path
-	@array: 		return array of whole data
-	'''
-	raster = gdal.Open(rasterfn)
-	band = raster.GetRasterBand(1)
-	if(isAlldata):
-		array = band.ReadAsArray()
-	else:
-		array = band.ReadAsArray(0, 0, 1, 1)
-	return array
-
 def fetch_bandInfo(band):
 	'''
 	Function: printout band information given a raster_band.
@@ -123,71 +109,6 @@ def fetch_bandInfo(band):
 
 	if( band.GetRasterColorTable() ):
 	    print("Band has a color table with {} entries".format(band.GetRasterColorTable().GetCount()))
-
-def get_SR_BandValues(ls_datainfo, row_start=0, row_end=0, col_start=0, col_end=0):
-	'''
-	Function: Construct SR_BandValues by for each data file in ls_datainfo.
-	@ls_datainfo: 		list data information by walking all files
-	@row_start: 		row start index of a data range
-	@row_end:			row end index of a data range
-	@col_start:			column start index of a data range
-	@col_end:			column end index of a data range
-	@SR_BandValues:		Save all extracted values and return
-	'''
-
-	## initial SR_BandValues to save results.
-	SR_BandValues = np.zeros((row_end-row_start,col_end-col_start, len(ls_datainfo), 9))
-
-	## initial date_index used to process data of a raster_file
-	date_index = 0
-	## for each data_info to get SR_BandValues
-	for data_info in ls_datainfo:
-		## extract key information
-		raster_file = data_info[0]
-		band_info = data_info[1]
-		Julian_Day = data_info[2]
-
-		## check if raster_file is existed and can open correctly
-		if os.path.exists(raster_file):
-			## open raster_file
-			ds = gdal.Open(raster_file)
-		
-		if ds is None:
-			print('Could not open {0}'.format(raster_file))
-		else:
-			## a) ---------- read band data ---------------
-			raster_band = ds.GetRasterBand(1)
-			## used for debug
-			# fetch_bandInfo(raster_band)
-
-			Band_Number = band_info[1]
-			## get point values given region[row_start:row_end,col_start:col_end]
-			for i in range(row_start, row_end):
-				for j in range(col_start,col_end):
-					## add Julian_Day to the last dimension of SR_BandValues
-					SR_BandValues[i - row_start, j-col_start, date_index, 0] = Julian_Day
-
-					## b) ----------- read a point value -------------		 
-					raster_point = raster_band.ReadAsArray(j, i, 1, 1)
-
-					## post-process data before saving into SR_BandValues
-					if(raster_point):
-						data_value = raster_point[0][0]
-					else:
-						## ????????how to handle empty or invid raster_point?
-						data_value = 0
-
-					## perform QA_Conversion
-					if( (Band_Number == len(band_info[2])) and (data_value > 0) ):
-						data_value = QA_Conversion(data_value)
-
-					## skip operation if Band_Number is not valid.
-					if(Band_Number>0):
-						SR_BandValues[i - row_start, j-col_start, date_index, Band_Number] = data_value
-
-		## increase date_index for next raster_file
-		date_index+=1
-	return SR_BandValues
 
 
 def get_bandinfo(raster_file):
@@ -213,82 +134,122 @@ def get_bandinfo(raster_file):
 
 	return [band, Band_Number, bandList]
 
-def get_datainfo(dataset_dir):
-	'''
-	Function: get data information by for each file within data directory.
-	@dataset_dir: 		directory path that saves all files
-	@datainfo:			return extracted data information as list [raster_file, band_info, Julian_Day]
-	'''
+'''
+Pre-process data class
+'''
+class Pre_Data(object):
+	@staticmethod
+	def get_datainfo(dataset_dir):
+		'''
+		Function: get data information by for each file within data directory.
+		@dataset_dir: 		directory path that saves all files
+		@datainfo:			return extracted data information as list [raster_file, band_info, Julian_Day]
+		'''
 
-	## get all data src path
-	ls_datapath = walkDirs(data_dir)
+		## get all data src path
+		ls_datapath = walkDirs(dataset_dir)
 
-	datainfo = []
-	## for each dataset folder
-	for datapath in ls_datapath:
-		## get list files within datapath
-		list_files = datapath[1]
+		datainfo = []
+		## for each dataset folder
+		for datapath in ls_datapath:
+			## get list files within datapath
+			list_files = datapath[1]
 
-		Julian_Day_Original = datapath[0].split('/')[-1]
-		Julian_Day = date_Conversion('20130415')
-		
-		## process each raster_file to get data infomation
-		for raster_file in list_files:
-			band_info = get_bandinfo(raster_file)
-			data_item = []
-			## set data_item
-			data_item.append(raster_file)
-			data_item.append(band_info)
-			data_item.append(Julian_Day)
+			Julian_Day_Original = datapath[0].split('/')[-1]
+			Julian_Day = date_Conversion('20130415')
+			
+			## process each raster_file to get data infomation
+			for raster_file in list_files:
+				band_info = get_bandinfo(raster_file)
+				data_item = []
+				## set data_item
+				data_item.append(raster_file)
+				data_item.append(band_info)
+				data_item.append(Julian_Day)
 
-			## append to datainfo
-			datainfo.append(data_item)
+				## append to datainfo
+				datainfo.append(data_item)
 
-	return datainfo
+		return datainfo
 
-def test_readArray(loadAlldata = False):
-	## ----------------- test readArray performance -------------------- 
-	start_time=time.time()
-	raster_file = "../VIIRS_ntl/201401/SVDNB_npp_20140101-20140131_00N060E_vcmslcfg_v10_c2015006171539.avg_rade9h.tif" 
-	raster_array=raster2array(raster_file, loadAlldata)
-	exec_time=time.time()-start_time
-	print("raster_array shape: {}".format(raster_array.shape))
-	print("Running time: {:.3f} s".format(exec_time))	
+	@staticmethod
+	def raster2array(rasterfn, isAlldata=True):
+		'''
+		Function: demo and test reading file functions of gdal.
+		@rasterfn: 		original raster file path
+		@array: 		return array of whole data
+		'''
+		raster = gdal.Open(rasterfn)
+		band = raster.GetRasterBand(1)
+		if(isAlldata):
+			array = band.ReadAsArray()
+		else:
+			array = band.ReadAsArray(0, 0, 1, 1)
+		return array
 
-def test_getSRvalues(data_dir):
-	## 1) for each file to get data information ['raster_file', [band, Band_Number, band_list], Julian_Day]
-	ls_datainfo=get_datainfo(data_dir)
-	# print(ls_datainfo[0])
+	@staticmethod
+	def get_SR_BandValues(ls_datainfo, row_start=0, row_end=0, col_start=0, col_end=0):
+		'''
+		Function: Construct SR_BandValues by for each data file in ls_datainfo.
+		@ls_datainfo: 		list data information by walking all files
+		@row_start: 		row start index of a data range
+		@row_end:			row end index of a data range
+		@col_start:			column start index of a data range
+		@col_end:			column end index of a data range
+		@SR_BandValues:		Save all extracted values and return
+		'''
 
-	## set data range
-	row_start=870
-	row_end=872
-	col_start=0
-	col_end=5
+		## initial SR_BandValues to save results.
+		SR_BandValues = np.zeros((row_end-row_start,col_end-col_start, len(ls_datainfo), 9))
 
-	## 2) Get SR_BandValues by for each ls_datainfo
-	start_time=time.time()
-	SR_BandValues = get_SR_BandValues(ls_datainfo, row_start, row_end, col_start, col_end)
-	print("SR_BandValues shape:{}".format(SR_BandValues.shape))
-	# print(SR_BandValues.T[0][0].T)
-	# print(SR_BandValues.T[0])
-	exec_time=time.time()-start_time
-	print("Running time: {:.3f} s".format(exec_time))
+		## initial date_index used to process data of a raster_file
+		date_index = 0
+		## for each data_info to get SR_BandValues
+		for data_info in ls_datainfo:
+			## extract key information
+			raster_file = data_info[0]
+			band_info = data_info[1]
+			Julian_Day = data_info[2]
 
+			## check if raster_file is existed and can open correctly
+			if os.path.exists(raster_file):
+				## open raster_file
+				ds = gdal.Open(raster_file)
+			
+			if ds is None:
+				print('Could not open {0}'.format(raster_file))
+			else:
+				## a) ---------- read band data ---------------
+				raster_band = ds.GetRasterBand(1)
+				## used for debug
+				if(date_index==0):
+					fetch_bandInfo(raster_band)
 
-if __name__ == "__main__":
+				Band_Number = band_info[1]
+				## get point values given region[row_start:row_end,col_start:col_end]
+				for i in range(row_start, row_end):
+					for j in range(col_start,col_end):
+						## add Julian_Day to the last dimension of SR_BandValues
+						SR_BandValues[i - row_start, j-col_start, date_index, 0] = Julian_Day
 
-	# test_readArray(False)
+						## b) ----------- read a point value -------------		 
+						raster_point = raster_band.ReadAsArray(j, i, 1, 1)
 
-	## data_dir = "/media/external/Deng/142041/LC081420412013041501T1-SC20191127190639"
-	data_dir = "/media/external/viirs_all"
-	## data_dir = "/media/external/Deng/142041"
-	# data_dir = "../VIIRS_ntl"
+						## post-process data before saving into SR_BandValues
+						if(raster_point):
+							data_value = raster_point[0][0]
+						else:
+							## ????????how to handle empty or invid raster_point?
+							data_value = 0
 
-	test_getSRvalues(data_dir)
+						## perform QA_Conversion
+						if( (Band_Number == len(band_info[2])) and (data_value > 0) ):
+							data_value = QA_Conversion(data_value)
 
+						## skip operation if Band_Number is not valid.
+						if(Band_Number>0):
+							SR_BandValues[i - row_start, j-col_start, date_index, Band_Number] = data_value
 
-	## 3) Calculate CCD points
-
-
-	pass
+			## increase date_index for next raster_file
+			date_index+=1
+		return SR_BandValues
--- GIS_DataAnalysis/proj_py/src/test.py
+++ GIS_DataAnalysis/proj_py/src/test.py
@@ -8,10 +8,12 @@ Created on Oct.9, 2019
 @TaskDescription: This module used for functions test.
 @Reference: 
 '''
-
+import time, sys
+import argparse
 from curve_validation import Curve_Validation
 from RF_Nepal import RF_Nepal
 from multi_processing import Multi_ProcessRF
+from Data_Preprocessing import Pre_Data
 
 def test_validation():
 	data_config= {}
@@ -78,13 +80,80 @@ def test_Multi_Process_RF():
 	Multi_ProcessRF.multi_processRF(data_config)
 
 
+def test_readArray(loadAlldata = False):
+	## ----------------- test readArray performance -------------------- 
+	start_time=time.time()
+	raster_file = "../VIIRS_ntl/201401/SVDNB_npp_20140101-20140131_00N060E_vcmslcfg_v10_c2015006171539.avg_rade9h.tif" 
+	raster_array=Pre_Data.raster2array(raster_file, loadAlldata)
+	exec_time=time.time()-start_time
+	print("raster_array shape: {}".format(raster_array.shape))
+	print("Running time: {:.3f} s".format(exec_time))	
 
+def test_getSRvalues(data_dir):
+	## 1) for each file to get data information ['raster_file', [band, Band_Number, band_list], Julian_Day]
+	ls_datainfo=Pre_Data.get_datainfo(data_dir)
+	# print(ls_datainfo[0])
+
+	## set data range
+	row_start=870
+	row_end=871
+	col_start=0
+	col_end=2851
+
+	## 2) Get SR_BandValues by for each ls_datainfo
+	start_time=time.time()
+	SR_BandValues = Pre_Data.get_SR_BandValues(ls_datainfo, row_start, row_end, col_start, col_end)
+	print("SR_BandValues shape:{}".format(SR_BandValues.shape))
+	# print(SR_BandValues.T[0][0].T)
+	# print(SR_BandValues.T[0])
+	exec_time=time.time()-start_time
+	print("Running time: {:.3f} s".format(exec_time))
+
+def define_and_get_arguments(args=sys.argv[1:]):
+    parser = argparse.ArgumentParser(
+        description="Run test evaulation app."
+    )
+    parser.add_argument("--test_func", type=int, default=0, 
+                        help="Execute test operation: \
+                        0-function test, \
+                        1-test_validation, \
+                        2-test_create_train_table, \
+                        3-test_RF_predict, \
+                        4-test_Multi_Process_RF, \
+                        5-test_Merge_predict, \
+                        6-test_Load_predict, \
+                        7-test_readArray, \
+                        8-test_getSRvalues")
+    parser.add_argument("--op_status", type=int, default=0, 
+                        help="Execute test based on condition.")
+
+    args = parser.parse_args(args=args)
+    return args
 
 if __name__ == "__main__":
-	# test_validation()
-	# test_create_train_table()
-	# test_RF_predict()
-	# test_Multi_Process_RF()
-	# test_Merge_predict()
-	# test_Load_predict()
-	pass
+	## define arguments for test app
+	args = define_and_get_arguments()
+
+	if(args.test_func==1):
+		test_validation()
+	elif(args.test_func==2):
+		test_create_train_table()
+	elif(args.test_func==3):
+		test_RF_predict()
+	elif(args.test_func==4):
+		test_Multi_Process_RF()
+	elif(args.test_func==5):
+		test_Merge_predict()
+	elif(args.test_func==6):
+		test_Load_predict()
+	elif(args.test_func==7):
+		test_readArray(True)
+	elif(args.test_func==8):
+		## data_dir = "/media/external/Deng/142041/LC081420412013041501T1-SC20191127190639"
+		data_dir = "/media/external/viirs_all"
+		## data_dir = "/media/external/Deng/142041"
+		# data_dir = "../VIIRS_ntl"
+
+		test_getSRvalues(data_dir)
+	else:
+		pass
